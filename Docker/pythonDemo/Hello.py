d85e1e10143419fb871c8c2c4b386bd541006e56
Piyush Kasture <110760778+piyushkasture@users.noreply.github.com>
2026-01-08 10:03:34 +0530

    Add files via upload

---
+++ Docker/pythonDemo/Hello.py
@@ -0,0 +1,11 @@
+# print("Hello")
+# print("This is demo from Docker..")
+# num1=input("Enter the first number: ")
+# num2=input("Enter the second number: ")
+num1=10
+num2=665
+
+if(num1>num2):
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
